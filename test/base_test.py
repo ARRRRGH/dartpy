b3c2c291d5c6b9b5f88497dc7bd78541fada1597
Unknown <rrrr@posteo.ch>
2019-10-15 21:13:48 +0200

    introduced tests

---
+++ test/base_test.py
@@ -0,0 +1,8 @@
+import simulation.simulation as simul
+
+def base_test():
+    sim = simul.Simulation('/Users/jim/Studium/REMOTE_SENSING/DART/dartpy/config_templates/base560.toml')
+
+
+if __name__ == '__main__':
+    base_test()
